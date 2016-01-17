43bc066ee53b64db12afbd9bbf6d1f7c21be3497
Kevan Lu <kevan.lu@mail.mcgill.ca>
2016-01-16 22:33:51 -0500

    Major Updates

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ gulp.task('autoprefixer', function () {
     var postcss      = require('gulp-postcss');
     var autoprefixer = require('autoprefixer-core');
 
-    return gulp.src('./_site/stylesheets/*.css')
+    return gulp.src('./_site/precompiled/*.css')
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-        .pipe(gulp.dest('./_site/stylesheets/'));
+        .pipe(gulp.dest('./stylesheets/'));
 });
 
 gulp.task('minify-css', function() {
     gulp.task('minify-css', function() {
-      return gulp.src('./_site/stylesheets/*.css')
+      return gulp.src('./_site/precompiled/*.css')
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./_site/stylesheets/'));
+        .pipe(gulp.dest('./stylesheets/'));
     });
 });
 
@@ -22,9 +22,9 @@ gulp.task('default', function() {
     var postcss      = require('gulp-postcss');
     var autoprefixer = require('autoprefixer-core');
 
-    return gulp.src('./_site/stylesheets/*.css')
+    return gulp.src('./_site/precompiled/*.css')
         .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
-        .pipe(gulp.dest('./_site/stylesheets/'))
+        .pipe(gulp.dest('./stylesheets/'))
         .pipe(minifyCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./_site/stylesheets/'));
+        .pipe(gulp.dest('./stylesheets/'));
 });
