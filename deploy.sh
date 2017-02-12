2c67f9ef7be30203662d343942e363a266e83215
Pavel Yarmak <pyarmak@gmail.com>
2017-02-11 21:16:21 -0600

    Travis CI key fix

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,7 @@ git clone $REPO serve
 cd serve
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
+ls
 
 # Clean out existing contents
 rm -rf serve/* || exit 0
@@ -56,7 +57,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
