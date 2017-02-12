65f281fa290b949c3d25dfe5947674ab30608f18
Pavel Yarmak <pyarmak@gmail.com>
2017-02-11 21:10:57 -0600

    Final Travis CI test

--- deploy.sh
+++ deploy.sh
@@ -28,25 +28,23 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf serve/**/* || exit 0
+rm -rf serve/* || exit 0
+mv serve/.git .backup
 
 # Run our compile script
 doCompile
 
 # Now let's go have some fun with the cloned repo
+mv .backup serve/.git
 cd serve
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-ls -laR
-git diff --exit-code
-echo "Exit code: "
-echo $?
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
+# if [ -z `git diff --exit-code` ]; then
+#     echo "No changes to the output on this push; exiting."
+#     exit 0
+# fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
