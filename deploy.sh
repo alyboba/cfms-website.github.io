d074028ac60abfcd7a96639a87d83eb5f3d10922
Pavel Yarmak <pyarmak@gmail.com>
2017-02-12 09:16:37 -0600

    Travis CI: Commit file removals as well

--- deploy.sh
+++ deploy.sh
@@ -49,8 +49,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
+git add -A
+git commit -m "Deploy website to GitHub: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
