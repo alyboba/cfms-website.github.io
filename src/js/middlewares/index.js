5dfb0a5afdd070ce215d53c16e9e0d1318c5dd0b
Pavel Yarmak <pyarmak@gmail.com>
2017-03-24 10:34:02 -0500

    Clean up old payment demo code

--- src/js/middlewares/index.js
+++ src/js/middlewares/index.js
@@ -1,11 +1,9 @@
 import NavigationMiddleware from './navigation';
 import AuthenticationMiddleware from './authentication';
-import PaymentMiddleware from './payments';
 
 export default class Middleware {
     constructor(page) {
         page('*', NavigationMiddleware);
         page('*', AuthenticationMiddleware);
-        page('/meetings/payment-demo.html', PaymentMiddleware);
     }
 }
--- src/js/middlewares/payments.js
+++
@@ -1,9 +0,0 @@
-import PaymentsController from '../controllers/payments';
-import PaymentsService from '../services/payments';
-import AuthenticationService from '../services/authentication';
-
-export default function(ctx, next) {
-    new PaymentsController({ authenticationService: new AuthenticationService(), paymentsService: new PaymentsService() });
-
-    next();
-}
