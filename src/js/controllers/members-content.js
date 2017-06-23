35c6bbf2c92768559705ed45e22dd250b1fc8817
Pavel Yarmak <pyarmak@gmail.com>
2017-06-23 14:51:46 -0500

    Members only content workflow complete
    Finished creating a workflow for storing and editingn secure member only content via firebase database and firepad for editing.
    
    See /resources/discounts-online.html for a prototypical implementation.

---
+++ src/js/controllers/members-content.js
@@ -0,0 +1,40 @@
+import { FirebaseConnection } from '../repositories/firebase/utils';
+
+export default class extends FirebaseConnection {
+    constructor(authenticationService) {
+        super();
+        this.auth = authenticationService;
+        this.membersOnly = document.getElementsByClassName('members-only');
+        if (this.membersOnly.length !== 0) this.process();
+    }
+
+    process() {
+        let editor = document.getElementById('edit-members-only');
+        if (editor && this.auth.user.isAdmin) return this.injectEditor(editor);
+        const refPath = 'members_only_content' + this.UrlToRef(); // TODO: DRY this crap up
+        const ref = this.firebase.database().ref(refPath);
+        const valRef = ref.child('/value');
+        valRef.once('value', snapshot => {
+            if (snapshot.val()) this.membersOnly[0].innerHTML = snapshot.val();
+        });
+    }
+
+    injectEditor(editor) {
+        const refPath = 'members_only_content' + this.UrlToRef();
+        const codeMirror = new CodeMirror(editor, { lineWrapping: true, lineNumbers: true, mode: 'htmlmixed' });
+        const ref = this.firebase.database().ref(refPath);
+        const valRef = ref.child('/value');
+
+        let firepad = Firepad.fromCodeMirror(ref, codeMirror);
+
+        firepad.on('synced', function(isSynced) {
+            if (isSynced) valRef.set(firepad.getText());
+        });
+
+        valRef.on('value', snapshot => document.getElementById('preview').innerHTML = snapshot.val());
+    }
+
+    UrlToRef() {
+        return window.location.pathname.split(".")[0];
+    }
+}
