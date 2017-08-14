9ccd22ad0e44330d6c0db0a8af82395a978c6246
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-08-14 15:00:39 -0500

    Iss31 - Fixed Bundle Js code for Pagination

--- src/js/controllers/pagination.js
+++ src/js/controllers/pagination.js
@@ -3,64 +3,59 @@
  */
 
 export default class PaginationController {
-	static getPaginationPage(index){
-		return this.paginationPages[index];
-	}
-	static getPaginationLink(index){
-		return this.paginationLinks[index];
-	}
-	static getPaginationPageLength(){
-		return this.paginationPages.length;
-	}
-	static setPaginationPages(input){
-		this.paginationPages = input;
-	}
-	static setPaginationLinks(input){
-		this.paginationLinks = input;
-	}
-	static setCurrentPage(input){
-		this.currentPage = input;
-	}
-	static getCurrentPage(){
-		return this.currentPage;
-	}
+
 	constructor() {
-		PaginationController.setPaginationPages(document.getElementsByClassName('paginationPages'));
-		PaginationController.setPaginationLinks(document.getElementsByClassName('paginationLink'));
-		PaginationController.setCurrentPage(1970);
+		this.paginationPages;
+		this.paginationLinks;
+		this.currentPage;
 		this.bindListeners();
 	}
-	
-	bindListeners() {
-		console.log("In the pagination Controller");
-		
-		
-		//Use eventListener
-		//pass the id into the function
-		
-		for(let i =0; i<PaginationController.getPaginationPageLength(); i++){
-			let button = PaginationController.getPaginationLink(i);
-			console.log(button);
-			button.onclick = this.showPage;
-			//console.log('the id is : ' + PaginationController.getPaginationPage(i).id);
-		}
-		//console.log("Hit the pagination controller");
-		//let currentPage = 1970;
+	getPaginationPage(index){
+		return this.paginationPages[index];
 	}
-	
+	getPaginationLink(index){
+		return this.paginationLinks[index];
+	}
+	getPaginationPageLength(){
+		return this.paginationPages.length;
+	}
+	setPaginationPages(input){
+		this.paginationPages = input;
+	}
+	setPaginationLinks(input){
+		this.paginationLinks = input;
+	}
+	setCurrentPage(input){
+		this.currentPage = input;
+	}
+	getCurrentPage(){
+		return this.currentPage;
+	}
+	bindListeners() {
+		this.setPaginationPages(document.getElementsByClassName('paginationPages'));
+		this.setPaginationLinks(document.getElementsByClassName('paginationLink'));
+		console.log("In the pagination Controller");
+		for(let i =0; i<this.getPaginationPageLength(); i++){
+			let button = this.getPaginationLink(i);
+			//console.log(button);
+			button.onclick = this.showPage;
+		}
+	}
 	showPage() {
 		event.preventDefault();
+		let paginationController = new PaginationController(); //Create an object so we can access the methods above.
 		let id = 'page'+this.id;
-		console.log(id);
-		PaginationController.setCurrentPage(this.id);
-		for(let i=0; i<PaginationController.getPaginationPageLength(); i++){
-			if(PaginationController.getPaginationPage(i).id == id){
-				PaginationController.getPaginationPage(i).classList.add('show');
-				PaginationController.getPaginationPage(i).classList.remove('hide');
+		let pageId = this.id;
+		//console.log(id);
+		paginationController.setCurrentPage(pageId);
+		for(let i=0; i<paginationController.getPaginationPageLength(); i++){
+			if(paginationController.getPaginationPage(i).id == id){
+				paginationController.getPaginationPage(i).classList.add('show');
+				paginationController.getPaginationPage(i).classList.remove('hide');
 			}
 			else {
-				PaginationController.getPaginationPage(i).classList.add('hide');
-				PaginationController.getPaginationPage(i).classList.remove('show');
+				paginationController.getPaginationPage(i).classList.add('hide');
+				paginationController.getPaginationPage(i).classList.remove('show');
 			}
 		}
 	}
