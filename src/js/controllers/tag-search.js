cee5eca38998421aec6c7644ad5e92e694216624
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-08-17 16:37:42 -0500

    Iss34 - Added Search Filter by Paper name logic & integrate into other 2 filters

--- src/js/controllers/tag-search.js
+++ src/js/controllers/tag-search.js
@@ -5,25 +5,25 @@ export default class TagSearchConroller {
 	
 	bindListeners(){
 		$(document).ready(function () {
-			var searchBar = $('<input style ="display:none;"id="searchFilter" type="text" name="search" placeholder="Search Paper by name With Filters">');
-			$("#filters").append(searchBar);
+			$("#noJavaScriptMessage").css({"display" : "none"}); //If user has javascript, hide this element. If they have no JS, User friendly message will display to them.
+			var searchBar = $('<input style ="display:none;"id="searchFilter" type="text" name="search" placeholder="Search paper name with filters">');
+			$("#filters").append(searchBar); //Code used to add element to page if user has javascript installed.
 			$("#searchFilter").css({"display" : "block"});
-			var selectedSection = "All Sections";
-			var currentSearchField = "";
-			//Copyright and credit given to http://kronosapiens.github.io/blog/2014/03/31/a-dynamic-and-generally-efficient-front-end-filtering-algorithm.html
-			//NO liscense on code, however credit due.
+			var selectedSection = "All Sections"; //Variable used to pass data between searchFilter, Checkbox Filter, and Selected option filter.
+			var currentSearchField = ""; //Variable used to pass data between the checkbox filters and typed in filters. Used to integrate the two together
+			
+			//Copyright and credit given to http://kronosapiens.github.io/blog/2014/03/31/a-dynamic-and-generally-efficient-front-end-filtering-algorithm.html.
 			$("#filters :checkbox").click(function () {
 				applyTagFilter();
-				applySearchFieldFilter(currentSearchField);
+				applySearchFieldFilter(currentSearchField); //passes substring still in input textbox to function
 				selectedSection = textBoxChange();
 				//This code is used to hide a section if all elements are filtered out.
 				checkSectionAfterFilter();
 			});
 			
 			$("#searchFilter").on("keyup", function () {
-				currentSearchField = $(this).val().toLowerCase();
-				applySearchFieldFilter(currentSearchField);
-				//applyTagFilter();
+				currentSearchField = $(this).val().toLowerCase(); //Grab the new substring entered
+				applySearchFieldFilter(currentSearchField); //pass the input to the function
 				selectedSection = textBoxChange();
 				checkSectionAfterFilter();
 			});
@@ -34,9 +34,11 @@ export default class TagSearchConroller {
 				checkSectionAfterFilter();
 			});
 			
+			/*
+			function that contains logic performed when user types into the search bar.
+			 */
 			function applySearchFieldFilter(searchBarFilter){
-				console.log("hitting search keyup)");
-				//iterate through all items containing class student-details, grabbing h3 to grab text of this variable.
+				//iterate through all paper titles, compare the search entry to the passed in parameter
 				$(".paper-title").each(function () {
 					var nameSearched = $(this).text().toLowerCase();
 					var $filteredElement = $(this).parent().parent();
@@ -53,6 +55,9 @@ export default class TagSearchConroller {
 				});
 			}
 			
+			/*
+			Function to hold logic for applying filtering to papers using Tagging system.
+			 */
 			function applyTagFilter() {
 				$(".filtered-content").hide();
 				var activeFilters = getActiveFilters();
@@ -63,9 +68,12 @@ export default class TagSearchConroller {
 						$paper.show();
 					}
 				});
-				
 			}
 			
+			/*
+			Function used to grab active filters. Utility function of 
+			//Copyright and credit given to http://kronosapiens.github.io/blog/2014/03/31/a-dynamic-and-generally-efficient-front-end-filtering-algorithm.html.
+			 */
 			function getActiveFilters() {
 				var filterArray = [];
 				$("#filters :checkbox:checked").each(function () {
@@ -73,6 +81,10 @@ export default class TagSearchConroller {
 				});
 				return filterArray;
 			}
+			/*
+			similar to above, Utility function of
+			 //Copyright and credit given to http://kronosapiens.github.io/blog/2014/03/31/a-dynamic-and-generally-efficient-front-end-filtering-algorithm.html.
+			 */
 			function lessonQualified(filters, lesson) {
 				for (var i = 0; i < filters.length; i++) {
 					if ((lesson.indexOf(filters[i])) == -1) {
@@ -82,6 +94,9 @@ export default class TagSearchConroller {
 				return true;
 			}
 			
+			/*
+			Function used to detect when select option dropdown box has been modified by end-user. Shows appropriate section of papers to end-user
+			 */
 			function textBoxChange() {
 				var selectedSection = $(":selected").text();
 				$(".paper-section").each(function() {
@@ -96,6 +111,9 @@ export default class TagSearchConroller {
 				return selectedSection;
 			}
 			
+			/*
+			A function that is called to check whether to show or hide a UX message "Nothing was found. Try widening your search criteria"
+			 */
 			function checkSectionAfterFilter() {
 				$(".paper-section").each(function(){
 					//$(this).parent().show();
@@ -111,13 +129,6 @@ export default class TagSearchConroller {
 				}
 				
 			}
-			
-			
-			
-
-			
-			
-			
 		});
 	}
 }
