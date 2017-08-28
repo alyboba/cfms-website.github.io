6012af251cf3e566e2e3bc42105cc1ec4fa3312e
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-08-28 13:36:09 -0500

    Iss50 - added Promise functionality to vex confirms

--- src/js/controllers/meeting-minutes.js
+++ src/js/controllers/meeting-minutes.js
@@ -7,6 +7,7 @@ export default class MeetingMinutesController extends FirebaseConnection{
 		super();
 		this.utils = new Utils();
 		this.ModalController = ModalController;
+		this.refPath = 'meeting-minutes/';
 		//meetingMinutesController;
 		this.auth = authenticationService;
 		this.process();
@@ -20,18 +21,18 @@ export default class MeetingMinutesController extends FirebaseConnection{
 			console.log("We are on page with a user signed in!");
 			var subRefPath = '';
 			var subSubRefPath = '';
-			var refPath = 'meeting-minutes/';
+			//var refPath = 'meeting-minutes/';
 			if(this.utils.isPageEnglish()){
-				refPath = refPath+'en';
+				this.refPath = this.refPath+'en';
 			}
 			else{
-				refPath = refPath+'fr';
+				this.refPath = this.refPath+'fr';
 			}
-			this.firebase.database().ref(refPath).on('value', (snapshot) => {
+			this.firebase.database().ref(this.refPath).on('value', (snapshot) => {
 				elem = '';
 				//subRefPath = refPath;
 				snapshot.forEach((childSnapshot) => {
-					subRefPath = refPath;
+					subRefPath = this.refPath;
 					subRefPath += '/'+childSnapshot.key;
 					//elem += '<blockquote>'; 
 					elem += '<h3 class="bold-red meetingMinuteYear">'+childSnapshot.key+'</h3>';
@@ -69,13 +70,13 @@ export default class MeetingMinutesController extends FirebaseConnection{
 					 //end admin if here
 					var updateButtons = document.getElementsByClassName("updateEntry");
 					for(let i=0; i< updateButtons.length; i++){
-						let updateButton = updateButtons[i];
-						updateButton.onclick = this.updateMeetingMinutes;
+						updateButtons[i].addEventListener('click', this.updateMeetingMinutesEvent.bind(this), false);
+						
 					}
 					var deleteButtons = document.getElementsByClassName("deleteEntry");
 					for(let i=0; i<deleteButtons.length; i++){
-						let deleteButton = deleteButtons[i];
-						deleteButton.onclick = this.deleteMeetingMinutes;
+						deleteButtons[i].addEventListener('click', this.deleteMeetingMinutesEvent.bind(this), false);
+						
 					}
 				}
 			});
@@ -106,9 +107,59 @@ export default class MeetingMinutesController extends FirebaseConnection{
 			console.log("We are on the page with user not signed in tsk tsk tsk.");
 		}
 	}
-	deleteMeetingMinutes(){
-		var dbPath = this.value;
+	deleteMeetingMinutesEvent(evt){
+		var dbPath = evt.target.value;
 		console.log(dbPath);
+		console.log(evt.target.value);
+		this.deleteMeetingMinutes(dbPath);
+	}
+	updateMeetingMinutesEvent(evt){
+		var utils = new Utils();
+		var modalController = this;
+		var dbPath = evt.target.value;
+		var title = evt.target.parentElement.getElementsByClassName("meetingMinuteTitle")[0].innerHTML;
+		var subTitle = evt.target.parentElement.getElementsByClassName("meetingMinuteSubTitle")[0].innerHTML;
+		var year = evt.target.parentElement.getElementsByClassName("meetingMinuteYear")[0].innerHTML;
+		vex.dialog.open({
+			message: 'Update Values',
+			input: [
+				'<input name="year" type="text" value="'+year+'" required/>',
+				'<input name="title" type="text" placeholder="'+title+'" required />',
+				'<input name="subTitle" type="text" placeholder="'+subTitle+'" required />'
+			].join(''),
+			buttons: [
+				$.extend({}, vex.dialog.buttons.YES, { text: 'Update' }),
+				$.extend({}, vex.dialog.buttons.NO, { text: 'Back' })
+			],
+			callback: function (data) {
+				if (!data) { //This executes if user clicks back.
+					console.log('Cancelled')
+				} else { //This executes if user clicks update.
+					//Add check that year is a year
+					if(data.year === year){
+						console.log("Executing when year == year!")
+						modalController.updateMeetingMinutes(dbPath, data.title, data.subTitle);
+						//Just update the other stuff....
+					}
+					else{
+						console.log("year does not = year.");
+						//Check if year entered is proper year
+						//Delete old entry @ year,
+						//add new entry into database....
+						if (modalController.isProperYearRange(data.year, 2000, 2030)) {
+							console.log("year is in correct format...");
+							modalController.addMeetingMinutes(data.year, data.title, data.subTitle, false);
+						} //end if
+						else {
+							vex.dialog.alert("Year must be between 2000 and 2030");
+						}
+					}
+				} //end else
+			} // end vex.open callback
+		});
+		
+	}
+	deleteMeetingMinutes(dbPath){
 		vex.dialog.confirm({
 			message: "Are you sure?",
 			callback: (value) =>{
@@ -124,67 +175,74 @@ export default class MeetingMinutesController extends FirebaseConnection{
 			}
 		});
 	}
-	updateMeetingMinutes(){
-		var utils = new Utils();
-		var dbPath = this.value;
-		var title = this.parentElement.getElementsByClassName("meetingMinuteTitle")[0].innerHTML;
-		var subTitle = this.parentElement.getElementsByClassName("meetingMinuteSubTitle")[0].innerHTML;
-		var year = this.parentElement.getElementsByClassName("meetingMinuteYear")[0].innerHTML;
-
+	
+	updateMeetingMinutes(dbPath, title, subTitle){
+		console.log(dbPath);
+		console.log(title);
+		console.log(subTitle);
+		vex.dialog.confirm({
+			message: "Are you sure?",
+			callback: (value) => {
+				if (value) {
+					//Add a check if year changed....
+					//If changed, delete old entry, add new entry with new year.
+					firebase.database().ref(dbPath).update({
+						title: title,
+						subTitle: subTitle,
+					}).then(() => {
+						document.getElementById('meetingMinutes').innerHTML = "";
+						vex.dialog.alert('<h3><strong>Success!</strong></h3>');
+					});
+				}
+				else {
+					console.log("clicked no");
+				}
+			} //end vex confirm callback
+		}); //end vex confirm
 		
-		vex.dialog.open({
-			message: 'Update Values',
-			input: [
-				'<input name="year" type="text" placeholder="'+year+'" required/>',
-				'<input name="title" type="text" placeholder="'+title+'" required />',
-				'<input name="subTitle" type="text" placeholder="'+subTitle+'" required />'
-			].join(''),
-			buttons: [
-				$.extend({}, vex.dialog.buttons.YES, { text: 'Update' }),
-				$.extend({}, vex.dialog.buttons.NO, { text: 'Back' })
-			],
-			callback: function (data) {
-				if (!data) {
-					console.log('Cancelled')
-				} else {
-					//Add check that year is a year
-					if(data.year > 2000 && data.year < 2031) {
-						vex.dialog.confirm({
-							message: "Are you sure?",
-							callback: (value) => {
-								if (value) {
-									if(data.year != year){ //user has updated the year, delete old entry, add new entry.
-										
-									}
-									else {
-										//Add a check if year changed....
-										//If changed, delete old entry, add new entry with new year.
-										firebase.database().ref(dbPath).update({
-											title: title,
-											subTitle: subTitle,
-										}).then(() => {
-											document.getElementById('meetingMinutes').innerHTML = "";
-											vex.dialog.alert('<h3><strong>Success!</strong></h3>');
-										});
-									}
-								}
-								else {
-									console.log("clicked no");
-								}
-							} //end vex confirm callback
-						}); //end vex confirm
-					} //end if
-					else{
-						vex.dialog.alert("Year must be between 2000 and 2030");
-					}
-				} //end else
-			} // end vex.open callback
+	}
+	addMeetingMinutes(year, title, subTitle, justAdd){
+		this.vexConfirm().then( () => {
+		console.log("The promise is true!");
+			firebase.database().ref(this.refPath+'/'+year).push({
+			title: title,
+			subTitle: subTitle
+		}).then( () => {
+			if(!justAdd){
+				console.log("Going to Delete old entry");
+			}
+			vex.dialog.alert('<h3><strong>Success!</strong></h3>');
+		});
+		}).catch( () => {
+			console.log("The promise returned false!!!!");
 		});
 		
-
-
-
+		console.log(this.refPath);
+	}
+	isProperYearRange(year, startYear, endYear){
+		return year > startYear && year < endYear;
 	}
 	
+	vexConfirm(){
+		return new Promise((resolve, reject) => {
+			vex.dialog.confirm({
+				message: "Are you sure?",
+				callback: (value) => {
+					if (value) {
+						resolve(true);
+					}
+					else {
+						reject(false);
+					}
+				} //end vex confirm callback
+			});
+		});
+		
+		
+	}
 	
+
 }
+
+
+
