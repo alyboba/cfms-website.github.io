63c4dd384026be17a80224319c3a94100381e9cb
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-09-08 15:51:09 -0500

    Iss50 -Hooked up logic to get Albums and Display in lightbox.

--- src/js/controllers/photo-gallery.js
+++ src/js/controllers/photo-gallery.js
@@ -18,68 +18,143 @@ export default class PhotoGalleryController{
 	
 	
 	process() {
+		let galleryController = this;
 		$(document).ready(function(){
 			// Load demo images from flickr:
 			$.ajax({
 				url: 'https://api.flickr.com/services/rest/',
 				data: {
 					format: 'json',
-					photoset_id: '72157688716575875',
 					user_id: '154596101@N02',
-					method: 'flickr.photosets.getPhotos',
-					//method: 'flickr.interestingness.getList',
+					method: 'flickr.photosets.getList',
 					api_key: '2360ff92cabc49f59a9ceaa136a0fbcb'
-					//api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line blueimp Key
 				},
 				dataType: 'jsonp',
 				jsonp: 'jsoncallback'
-			}).done(function (result) {
-				console.log(result);
-				var carouselLinks = []
-				var linksContainer = $('#links')
-				var baseUrl
+			}).done( (result) => {
+				var albumLinks = [];
+				var albumContainer = $('#albumContainer');
+				let albumElem;
+				//console.log(result);
+				var baseUrl;
 				
-				// Add the demo images as links with thumbnails to the page:
-				$.each(result.photoset.photo, function (index, photo) {
-				//$.each(result.photos.photo, function (index, photo) {
-					baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
-						photo.server + '/' + photo.id + '_' + photo.secret
-					$('<a/>')
-						.append($('<img>').prop('src', baseUrl + '_s.jpg'))
-						.prop('href', baseUrl + '_b.jpg')
-						.prop('title', photo.title)
-						.attr('data-gallery', '')
-						.appendTo(linksContainer);
-					carouselLinks.push({
-						href: baseUrl + '_c.jpg',
-						title: photo.title
-					});
-				});
-				// Initialize the Gallery as image carousel:
-				blueimp.Gallery(carouselLinks, {
-					container: '#blueimp-image-carousel',
-					carousel: true
-				})
-			});
+				
+				
+				$.each(result.photosets.photoset, (index, photo) => {
+					baseUrl = 'https://c1.staticflickr.com/' + photo.farm  +'/'+
+						photo.server + '/' + photo.primary + '_' + photo.secret;
+					//var photoId = photo.id;
+					//console.log(photo);
+					albumElem = '<div class="photoAlbum view photo-list-album-view awake" id="'+photo.id+'" style="transform: translate(0px, 8px);width: 240px;height: 240px;background-image:url('+baseUrl+'_n.jpg)">';
+					albumElem += '<a class="interaction-view avatar photo-list-album album ginormous" href="#" title="'+photo.title._content+'" data-rapid_p="65">';
+					albumElem += '<div class="photo-list-album-interaction dark has-actions" data-albumid="'+photo.id+'" >';
+					albumElem += '<a class="overlay" href="#" data-rapid_p="87"></a>';
+					albumElem += '<div class="interaction-bar">';
+					albumElem += '<div class="metadata">';
+					albumElem += '<h4 class="album-title">'+photo.title._content+'</h4>';
+					var photoString;
+					if(photo.photos>1){
+						photoString = "photos";
+					}else{
+						photoString = "photo";
+					}
+					albumElem += '<span class="album-photo-count secondary">'+photo.photos+' '+photoString+'</span>';
+					albumElem += '</div></div></div></a></div>';
+					
+					
+					$('#albumContainer').append(albumElem);
+					albumElem = "";
+				});//end Each
+				
+				$('.photoAlbum').click(function(event){
+					event.preventDefault();
+					console.log(this.id);
+					console.log("An album was clicked!");
+					//console.log(event);
+					
+					//Will put this in onClick method.
+					$.ajax({
+						url: 'https://api.flickr.com/services/rest/',
+						data: {
+							format: 'json',
+							photoset_id: this.id,
+							user_id: '154596101@N02',
+							method: 'flickr.photosets.getPhotos',
+							//method: 'flickr.interestingness.getList',
+							api_key: '2360ff92cabc49f59a9ceaa136a0fbcb'
+							//api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line blueimp Key
+						},
+						dataType: 'jsonp',
+						jsonp: 'jsoncallback'
+					}).done(function (result) {
+						console.log(result);
+						var carouselLinks = []
+						var linksContainer = $('#links')
+						var baseUrl
+						// Add the demo images as links with thumbnails to the page:
+						$.each(result.photoset.photo, function (index, photo) {
+							//$.each(result.photos.photo, function (index, photo) {
+							baseUrl = 'https://farm' + photo.farm + '.static.flickr.com/' +
+								photo.server + '/' + photo.id + '_' + photo.secret
+							$('<a/>')
+								.append($('<img>').prop('src', baseUrl + '_s.jpg'))
+								.prop('href', baseUrl + '_b.jpg')
+								.prop('title', photo.title)
+								.attr('data-gallery', '')
+								.appendTo(linksContainer);
+							carouselLinks.push({
+								href: baseUrl + '_c.jpg',
+								title: photo.title
+							});
+						});
+						// Initialize the Gallery as image carousel:
+						blueimp.Gallery(carouselLinks, {
+							container: '#blueimp-image-carousel',
+							carousel: true
+						})
+					}); //End ajax call.
+					
+					
+					//Hooking up lightBox links to onclick function
+					document.getElementById('links').onclick = function (event) {
+						event = event || window.event;
+						var target = event.target || event.srcElement,
+							link = target.src ? target.parentNode : target,
+							options = {index: link, event: event},
+							links = this.getElementsByTagName('a');
+						blueimp.Gallery(links, options);
+					}; //end hooking up lightbox onclick.
+					
+					
+				});//End onClick.
+				
+			}); //End get photo Albums ajax call
+			
+			
+			
+
 			
 			
 			
 			
-			//Hooking up lightBox links to onclick function
-			document.getElementById('links').onclick = function (event) {
-				event = event || window.event;
-				var target = event.target || event.srcElement,
-					link = target.src ? target.parentNode : target,
-					options = {index: link, event: event},
-					links = this.getElementsByTagName('a');
-				blueimp.Gallery(links, options);
-			};
-			
-			
-});
-		
-		
+		}); //End document.ready
 	}
+	
+	bubbleSortAlbums(arr){
+		let len = arr.length;
+		for (let i = len-1; i>=0; i--){
+			for(let j = 1; j<=i; j++){
+				if(arr[j-1].date_update >arr[j].date_update){
+					var temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	
 	
 	
 	
