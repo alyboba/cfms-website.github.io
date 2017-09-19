2a4d7c0c37a05ddf43eeebb3fee9ebae24d6d46d
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-09-19 14:52:21 -0500

    Iss #51 - bit of refactoring, added french page

--- src/js/controllers/photo-gallery.js
+++ src/js/controllers/photo-gallery.js
@@ -8,18 +8,12 @@ export default class PhotoGalleryController{
 	constructor() {
 		this.process();
 	}
-		
 	
-	//documentation for api methods
+	//documentation for api methods used in process
 	//https://www.flickr.com/services/api/explore/flickr.photosets.getPhotos
-	
 	
 	//To get album list
 	//https://www.flickr.com/services/api/explore/flickr.photosets.getList
-	
-	//To get photo Tags.
-	//https://www.flickr.com/services/api/explore/flickr.tags.getListUser
-	
 	process() {
 		let galleryController = this;
 		$(document).ready(function(){
@@ -43,18 +37,15 @@ export default class PhotoGalleryController{
 						$('.loader').hide();
 					}
 					else {
-						var albumLinks = [];
-						var albumContainer = $('#albumContainer');
+						let albumContainer = $('#albumContainer');
 						let albumElem;
-						//console.log(result);
-						var baseUrl;
-						var tagArray = [];
-						var addToTagArray = true;
+						let baseUrl;
+						let selectYearArray = [];
+						let addToTagArray = true;
 						$.each(result.photosets.photoset, (index, photo) => {
 							baseUrl = 'https://c1.staticflickr.com/' + photo.farm + '/' +
 								photo.server + '/' + photo.primary + '_' + photo.secret;
-							//var photoId = photo.id;
-							//console.log(photo);
+							//Building the album image containers dynamically through ajax call.
 							albumElem = '<li><div class="photoAlbum view photo-list-album-view awake" id="' + photo.id + '" style="transform: translate(0px, 8px);width: 240px;height: 240px;background-image:url(' + baseUrl + '_n.jpg)">';
 							albumElem += '<a class="interaction-view avatar photo-list-album album ginormous" href="#" title="' + photo.title._content + '" data-rapid_p="65">';
 							albumElem += '<div class="photo-list-album-interaction dark has-actions" data-albumid="' + photo.id + '" >';
@@ -62,23 +53,22 @@ export default class PhotoGalleryController{
 							albumElem += '<div class="interaction-bar">';
 							albumElem += '<div class="metadata">';
 							albumElem += '<h4 class="albumTitle">' + photo.title._content + '</h4>';
-							var albumTags = photo.description._content;
-							var albumTagArray = albumTags.split(',');
+							let albumTags = photo.description._content;
+							let albumTagArray = albumTags.split(',');
 							
-
-							for(var i=0; i<albumTagArray.length; i++){
+							for(let i=0; i<albumTagArray.length; i++){
 								if(albumTagArray[i].includes("year:")){
 									let subAlbumArray = albumTagArray[i].split(":");
 									albumElem += '<h4 class="albumYear">' + subAlbumArray[1].trim() + '</h4>';
-									for(var j=0; j<tagArray.length; j++){
-										if(subAlbumArray[1].trim().toUpperCase() === tagArray[j].toUpperCase()){
+									for(let j=0; j<selectYearArray.length; j++){
+										if(subAlbumArray[1].trim().toUpperCase() === selectYearArray[j].toUpperCase()){
 											addToTagArray = false;
 											break;
 										}
 										addToTagArray = true;
 									}
 									if(addToTagArray){
-										tagArray.push(subAlbumArray[1].trim());
+										selectYearArray.push(subAlbumArray[1].trim());
 									}
 								}
 								else if(albumTagArray[i].includes("school:")){
@@ -91,11 +81,9 @@ export default class PhotoGalleryController{
 								}
 								else {
 									albumElem += '<h4 class="albumTag" style="display:none;">' + albumTagArray[i].trim() + '</h4>';
-
 								}
 							}
-							
-							var photoString;
+							let photoString;
 							if (photo.photos > 1) {
 								photoString = "photos";
 							} else {
@@ -106,23 +94,19 @@ export default class PhotoGalleryController{
 							$('#list').append(albumElem);
 							albumElem = "";
 						});//end Each
-						tagArray = galleryController.bubbleSortArray(tagArray);
+						//Sorting years into order from least to greatest.
+						selectYearArray = galleryController.bubbleSortArray(selectYearArray);
 						//Add bubble sort method here.
-						
-						for(var i=0; i<tagArray.length; i++){
-							console.log(tagArray[i]);
-							var optionElem = '<option value ="'+tagArray[i]+'">'+tagArray[i]+'</option>';
+						for(let i=0; i<selectYearArray.length; i++){
+							let optionElem = '<option value ="'+selectYearArray[i]+'">'+selectYearArray[i]+'</option>';
 							$('#filter-tags').append(optionElem);
 						}
-						
-						
+											
 						$('.photoAlbum').click(function (event) {
 							event.preventDefault();
 							$('#closeButton').show("slow");
 							$('#albumContainer').hide("slow");
 							$('.loader').show("fast");
-							
-							//Will put this in onClick method.
 							$.ajax({
 								url: 'https://api.flickr.com/services/rest/',
 								data: {
@@ -130,9 +114,7 @@ export default class PhotoGalleryController{
 									photoset_id: this.id,
 									user_id: '154596101@N02',
 									method: 'flickr.photosets.getPhotos',
-									//method: 'flickr.interestingness.getList',
 									api_key: '2360ff92cabc49f59a9ceaa136a0fbcb'
-									//api_key: '7617adae70159d09ba78cfec73c13be3' // jshint ignore:line blueimp Key
 								},
 								dataType: 'jsonp',
 								jsonp: 'jsoncallback'
@@ -143,9 +125,9 @@ export default class PhotoGalleryController{
 								}
 								else {
 									//console.log(result);
-									var carouselLinks = []
-									var linksContainer = $('#links').empty();
-									var baseUrl
+									let carouselLinks = [],
+									linksContainer = $('#links').empty(),
+									baseUrl;
 									// Add the demo images as links with thumbnails to the page:
 									$.each(result.photoset.photo, function (index, photo) {
 										//$.each(result.photos.photo, function (index, photo) {
@@ -175,12 +157,10 @@ export default class PhotoGalleryController{
 								console.log("The inner request failed with");
 								console.log(error);
 							}); //End ajax call.
-							
-							
 							//Hooking up lightBox links to onclick function
 							document.getElementById('links').onclick = function (event) {
 								event = event || window.event;
-								var target = event.target || event.srcElement,
+								let target = event.target || event.srcElement,
 									link = target.src ? target.parentNode : target,
 									options = {index: link, event: event},
 									links = this.getElementsByTagName('a');
@@ -188,18 +168,18 @@ export default class PhotoGalleryController{
 							}; //end hooking up lightbox onclick.
 						});//End onClick.
 						//Add search etc here?
+						//Grabbing the list.js script dynamically after sucessfully hit flickr api.
 						$.getScript("//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js").done(() =>{
-							console.log("loaded list.js");
-							var monkeyList = new List('albumContainer', {
+							//This instantiates our pagination and search functionality 
+							let monkeyList = new List('albumContainer', {
 								valueNames: ['albumTitle', 'albumYear', 'albumSchool', 'albumTag', 'albumEvent'],
 								page: 6,
 								pagination: true
 							});
 							$('#filter-tags').change(function() {
 								console.log($('#filter-tags').val());
-								var selectedOption = $('#filter-tags').val();
+								let selectedOption = $('#filter-tags').val();
 								monkeyList.filter(function(item) {
-									console.log(item);
 									if(selectedOption === "Select Year"){
 										return true;
 									}
@@ -211,12 +191,11 @@ export default class PhotoGalleryController{
 								});
 								return false;
 							});
-							
 						});
 						$('.loader').hide();
 					} //End else (if no error)
 				}).fail( (error) =>{
-					console.log("the request failed and errored");
+					console.log("the request failed and threw an error");
 					console.log(error);
 				}); //End get photo Albums ajax call
 			
@@ -232,11 +211,11 @@ export default class PhotoGalleryController{
 	}//end process
 	
 	bubbleSortArray(array){
-		var length = array.length;
-		for(var i=0; i<length; i++){
-			for(var j=0; j<(length -i -1); j++){
+		let length = array.length;
+		for(let i=0; i<length; i++){
+			for(let j=0; j<(length -i -1); j++){
 				if(array[j] > array[j+1]){
-					var temp = array[j];
+					let temp = array[j];
 					array[j] = array[j+1];
 					array[j+1] = temp;
 				}
@@ -244,6 +223,4 @@ export default class PhotoGalleryController{
 		}
 		return array;
 	}
-		
-	
 }
