89ace5e1c22acd401d2a420cd4f708faec153919
Travis CI <it@cfms.org>
2017-09-21 00:21:08 +0000

    Deploy website to GitHub: 78b1d7d6619e4c9c72c139fd9e40b9cceb5c6e25

--- js/bundle.js
+++ js/bundle.js
@@ -120234,9 +120234,9 @@ var PhotoGalleryController = function () {
 					url: 'https://api.flickr.com/services/rest/',
 					data: {
 						format: 'json',
-						user_id: '154596101@N02',
+						user_id: '151536734@N03',
 						method: 'flickr.photosets.getList',
-						api_key: '2360ff92cabc49f59a9ceaa136a0fbcb'
+						api_key: '0de69094c4a08c0ec198f6e200681d2e'
 					},
 					dataType: 'jsonp',
 					jsonp: 'jsoncallback'
@@ -120320,9 +120320,9 @@ var PhotoGalleryController = function () {
 								data: {
 									format: 'json',
 									photoset_id: this.id,
-									user_id: '154596101@N02',
+									user_id: '151536734@N03',
 									method: 'flickr.photosets.getPhotos',
-									api_key: '2360ff92cabc49f59a9ceaa136a0fbcb'
+									api_key: '0de69094c4a08c0ec198f6e200681d2e'
 								},
 								dataType: 'jsonp',
 								jsonp: 'jsoncallback'
