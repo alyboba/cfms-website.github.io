f85c28aa8f5bd79379d36bb74c065ad1ab5599cc
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-11-01 11:49:30 -0500

    Iss#44 - Committing before some refactoring

--- src/js/controllers/communiques.js
+++ src/js/controllers/communiques.js
@@ -5,11 +5,22 @@ export default class CommuniquesController extends FirebaseConnection {
 	constructor({ authenticationService }) {
 		super();
 		this.utils = new Utils();
+		this.refPath = 'communiques/';
+		this.containerId = 'communiqueInformation';
 		this.service = authenticationService;
 		this.process();
 	}
 	
 	process(){
-		console.log("HI from communiques controller!");
-	}
+		if(this.auth.user){ //check if user signed in
+			
+			
+			
+			
+			
+		} //end if auth user
+		else{
+			console.log("not signed in");
+		}
+	} // end process()
 }
