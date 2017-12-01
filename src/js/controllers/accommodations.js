4cd087d408761e1e528afca51392ea854181396e
Justin Schellenberg <jrschellenberg@Gmail.com>
2017-12-01 13:04:47 -0600

    Added DataTable dependency to controller.

--- src/js/controllers/accommodations.js
+++ src/js/controllers/accommodations.js
@@ -1,8 +1,8 @@
 
 
-
 export default class AccommodationsController {
 	constructor(listing, province) {
+		
 		this.listing = listing;
 		this.province = province
 		this.process();
@@ -10,12 +10,9 @@ export default class AccommodationsController {
 	}
 	
 	process(){
-		$(document).ready(() => {
-			console.log($('#accommodationsDatabase'))
-			$('#accommodationsDatabase').DataTable( {
-				"pagingType": "full_numbers"
-			});
-		});
+		$('#accommodationsDatabase').DataTable();
+		
+		
 		console.log("hello from accommodationsController!");
 		console.log(`Listing is ${this.listing}`);
 		console.log(`Province is ${this.province}`);
