8340674b844b2555e8cbd6bbc0798b9174301a57
Adel Arezki <adel.arezki@mail.mcgill.ca>
2018-08-30 18:25:42 -0400

    research highlights 2

--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -76,6 +76,7 @@ export default class Router extends Middleware {
         route('/sitemap.html', SiteMap);
 
         route('/what-we-do/global-health/snapvocacy.html', Snapvocacy);
+        route('/what-we-do/education/research-highlights.html', ResearchHighlights);
     }
 
     refresh() {
